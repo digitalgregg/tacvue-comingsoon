6486be988c18d3afa5e8448869a9045ca77b4c98
sebastian lueders <sebastianjlueders@gmail.com>
2022-07-03 12:15:33 -0500

    Update OurVision.tsx
    Vision Statement syntax adjustment

--- components/Blueprint/OurVision.tsx
+++ components/Blueprint/OurVision.tsx
@@ -10,8 +10,8 @@ const OurVision = () => {
           <h2 className={cx(title_sm, text_gradient, "pt-20")}>OUR VISION</h2>
           <p className="mt-[47px] font-base sm:w-[80%] md:w-[650px] 2xl:w-[738px] flex mx-auto sm:text-[18px] md:text-[20px] lg:text-[22px] 2xl:text-[26px] xl:text-[20px] font-normal tracking-widest leading-[200%]">
             TacVue&apos;s mission is to power the individual to engage with an
-            open and accessible web3. We do not follow models, we develop them.
-            We build for the future, not the present.
+            open and accessible web3. <br>We do not follow models, we develop them.</br>
+            <br>We build for the future, not the present.</br>
           </p>
         </div>
       </div>
