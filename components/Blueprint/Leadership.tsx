55ea8f1befcba3fa99467c25894d6118a6e50ccb
sebastian lueders <sebastianjlueders@gmail.com>
2022-07-03 17:00:45 -0500

    Update Leadership.tsx
    Update to the Leadership Principles.

--- components/Blueprint/Leadership.tsx
+++ components/Blueprint/Leadership.tsx
@@ -8,21 +8,21 @@ const Leadership = () => {
       id: 1,
       title: "Customer Obsessed",
       description:
-        "Every business commitment we make is in our members' best interest. We take pride in looking at every decision from our customer's point of view before taking action.",
+        "Every business commitment our members make is in our community's best interest. Every decision that impacts the community should be visualized from the community's point of view before any action is taken.",
       img: "/images/customer-centric.svg",
     },
     {
       id: 2,
       title: "Agility over Size",
       description:
-        "We firmly believe that the future of our team is decentralized and autonomous. We strive to deliver the necessary tooling for large remote organizations to operate as efficiently as smaller centralized teams but until the right tools are developed, we must stick to our core belief: frugality beats indulgence.",
+        "We firmly believe that the future of TacVue is decentralized and autonomous. Until then, we stick to our core belief that frugality outlasts indulgence.",
       img: "/images/agility.svg",
     },
     {
       id: 3,
       title: "Own your Contribution",
       description:
-        "Every Entity holder is expected to follow leadership principles. Members are expected to treat their peers with respect and TacVue infrastructure as their own. We hope that every Entity holder takes pride in their contributions to the community, project and platform as our core team members would. ",
+        "Every Entity holder is encouraged to follow leadership principles. Members are expected to treat their peers with respect and the TacVue infrastructure as an extension of themselves.",
       img: "/images/own-your-contribution.svg",
     },
     {
@@ -34,30 +34,30 @@ const Leadership = () => {
     },
     {
       id: 5,
-      title: "Creativity over Tradition",
+      title: "Engagement is Value",
       description:
-        "Failure to innovate, advise or create is considered a failure to provide the TacVue community with value.",
+        "Failure to guide, innovate or create is considered a failure to provide the TacVue community with value.",
       img: "/images/creativity-over-templates.svg",
     },
     {
       id: 6,
       title: "Exude Confidence",
       description:
-        "TacVue members must be confident in their ability to receive criticism, reassess their decision-making and determine new paths forward without doubting their own abilities in the case of failure",
+        "TacVue members must be confident in their ability to receive criticism, reassess their decision-making and determine new paths forward without fear of failure.",
       img: "/images/expect-failures.svg",
     },
     {
       id: 7,
-      title: "The 51% Rule",
+      title: "Politics are Inefficient",
       description:
-        "Success is a state of mind, not a quantitative measure. Strive to act in the community's best interest while also vocalizing your unpopular opinions if you believe they are warranted. Leaders serve the community's best interest without playing politics.",
+        "Success is a state of mind, not a quantitative measure. Strive to act in the community's best interest, but also vocalize your unpopular opinions if you believe they're warranted. Leaders serve the community's best interest with honesty, not politics.",
       img: "/images/51-rule.svg",
     },
     {
       id: 8,
-      title: "Go the Extra Mile, Then Go Two More",
+      title: "Go the Extra Distance",
       description:
-        "TacVue members are encouraged to push the boundaries of what's possible by questioning everything on the platform. Rather than ask what something is, you should ask yourself why it exists.",
+        "TacVue members are encouraged to push the boundaries of what's possible by questioning every piece of the platform. Rather than ask what something is, they should ask why it exists.",
       img: "/images/curiousity-beats-compliance.svg",
     },
 
@@ -65,29 +65,29 @@ const Leadership = () => {
       id: 9,
       title: "Pursue Perfection",
       description:
-        "Perfection is impossible but it's what drives our community towards the best possible outcome of every situation. Although it's not attainable, it's what drives us.",
+        "True perfection is impossible to accomplish but if our members strive for it, the outcome will always be advantageous.",
       img: "/images/Group 37693.svg",
     },
     {
       id: 10,
-      title: "Think Huge",
+      title: "Think Cosmic",
       description:
-        "All TacVue members are encouraged to push their projects towards immense goals, relentlessly and unconditionally.",
+        "All TacVue members are encouraged to relentlessly push their projects towards monumental goals.",
       img: "/images/VectorProfile.svg",
     },
 
     {
       id: 11,
-      title: "Transparency is Bound to Trust",
+      title: "Transparency builds Trust",
       description:
-        "The best way to accomplish an immense goal as a community is through transparent communication. Although transparency does not guarantee trust, our community members should put trust before compassion.",
+        "Trust is essential to a community's success. We encourage our members to be transparent because we believe it's essential to sustainable collaboration.",
       img: "/images/GroupVectorScall.svg",
     },
     {
       id: 12,
-      title: "Add More Value Than You Take",
+      title: "Contribute More",
       description:
-        "In order to compete with centralized brands, project communities must collaborate with the intention of providing more value than they receive. The projects that accomplish this will have the potential to move mountains.",
+        "In order to compete with established brands, project members need to collaborate with the intention of providing more value than they receive.",
       img: "/images/breeds.svg",
     },
   ];
